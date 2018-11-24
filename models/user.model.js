b32e4a6cb9f785720429afba584b6bc75178d620
Hung Le <skvn.clew@gmail.com>
2018-11-25 00:22:43 +0700

    change bcrypt to ver1.0.3

--- models/user.model.js
+++ models/user.model.js
@@ -1,27 +1,32 @@
 const mongoose = require('mongoose');
 const { hash, compare } = require('bcrypt');
+// const bcrypt = require("bcrypt")
 mongoose.set('useCreateIndex', true);
 mongoose.connect('mongodb://localhost/asd123',{ useNewUrlParser: true });
-
+// mongoose.connect('mongodb://localhost/asd123',{useMongoClient: true});
 mongoose.Promise = global.Promise;
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    email: { type: String, required: true, unique: true, trim: true },
-    password: { type: String, required: true, trim: true},
-    name: { type: String, required: true },
-    phone: { type: String, required: true },
-    avatar: { type: String, required: true}
+    email: { type: String, required: true, unique: true, trim: true,index: true },
+    password: { type: String, required: true, trim: true,index: true },
+    name: { type: String, required: true,index: true },
+    phone: { type: String, required: true,index: true },
+    avatar: { type: String, required: true,index: true }
 });
+
 
 const UserModel = mongoose.model('User', userSchema);
 
 class User extends UserModel {
-    static async signUp(email, password, name, phone, avatar) {
+
+
+    static async signUp(email, password, name, phone, avatar)  {
         const encrypted = await hash(password, 8);
-        const user = new UserModel({ email, password: encrypted,name, phone, avatar });
+        const user = new UserModel({ name, email, password: encrypted, phone, avatar });
         return user.save();
     }
+
     static async signIn(email, password) {
         const user = await User.findOne({ email });
         if (!user) throw new Error('Cannot find user.');
